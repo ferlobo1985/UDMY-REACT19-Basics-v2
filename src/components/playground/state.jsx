47a67de8532e0b2ff67d80a7c6f1c26ca1fdb722
Coding rev <fernando.lobo.prez@gmail.com>
2025-03-05 01:34:05 -0300

    13-Complex state
    13-Complex state

--- src/components/playground/state.jsx
+++ src/components/playground/state.jsx
@@ -4,9 +4,9 @@ const StatePlayground = () => {
     const initialCount = 0;
     let [count,setCount] = useState(initialCount);
 
-    // const addOne = () => {
-    //     setCount(count+1)
-    // }
+    const addOne = () => {
+        setCount(count+1)
+    }
 
     const subtOne = () => {
         setCount(prevCount=> { 
@@ -20,12 +20,11 @@ const StatePlayground = () => {
     }
 
     return(
-        <>
+        <>  
             <h3>Count:{count}</h3>
-            <button onClick={()=> setCount(count+1)}>Add one +1</button>
+            <button onClick={addOne}>Add one +1</button>
             <button onClick={subtOne}>Subt one -1</button>
             <button onClick={resetCount}>RESET</button>
-
         </>
     )
 }
