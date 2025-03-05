8b524193e63d9d7d89106cfcddc93ba1892ad083
Coding rev <fernando.lobo.prez@gmail.com>
2025-03-05 02:10:31 -0300

    15-Props
    15-Props

---
+++ src/components/news_list.jsx
@@ -0,0 +1,10 @@
+const NewsList = (props) => {
+    console.log(props)
+    return(
+        <>
+            <div>New list:</div>
+        </>
+    )
+}
+
+export default NewsList;
--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,23 @@
 import { createRoot } from 'react-dom/client'
+import { useState } from 'react'
 import Header from './components/header/header'
 import StatePlayground from './components/playground/state'
+import NewsList from './components/news_list'
+import { newsData } from './utils/data'
 import './styles/styles.css'
 
-const App = () => (
-    <>
-        <Header/>
-        <div className='container'>
-            {/* <StatePlayground/> */}
-        </div>
-    </>
-)
+const App = () => {
+    let [news, setNews] = useState(newsData);
+
+    return(
+        <>
+            <Header/>
+            <div className='container'>
+                <NewsList news={news}/>
+            </div>
+        </>
+    )
+}
 
 createRoot(document.getElementById('root')).render(
     <App/>
