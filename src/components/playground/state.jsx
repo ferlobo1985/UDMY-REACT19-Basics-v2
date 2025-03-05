e7969b98331935969b3285dc89d5721ae6846ebd
Coding rev <fernando.lobo.prez@gmail.com>
2025-03-05 01:11:38 -0300

    12-Prev State
    12-Prev State

--- src/components/playground/state.jsx
+++ src/components/playground/state.jsx
@@ -1,16 +1,31 @@
 import { useState } from "react";
 
 const StatePlayground = () => {
-    let [count,setCount] = useState(0);
+    const initialCount = 0;
+    let [count,setCount] = useState(initialCount);
 
     // const addOne = () => {
     //     setCount(count+1)
     // }
 
+    const subtOne = () => {
+        setCount(prevCount=> { 
+            /// large calculation
+            return prevCount-1
+        })
+    }
+
+    const resetCount = () => {
+        setCount(initialCount)
+    }
+
     return(
         <>
             <h3>Count:{count}</h3>
             <button onClick={()=> setCount(count+1)}>Add one +1</button>
+            <button onClick={subtOne}>Subt one -1</button>
+            <button onClick={resetCount}>RESET</button>
+
         </>
     )
 }
